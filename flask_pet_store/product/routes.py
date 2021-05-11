73032106ba1701d3bea2f3130378763ed5d324e7
prachurya deepta <deeptoadhikary@gmail.com>
2021-05-11 00:55:27 -0400

    added images to carousel

--- flask_pet_store/product/routes.py
+++ flask_pet_store/product/routes.py
@@ -17,17 +17,17 @@ def index():
     q = request.args.get('q', default=None, type=str)
 
     search_form = SearchForm(search_query=q)
-    
+
     if search_form.validate_on_submit():
         return redirect(url_for('products.index', q=search_form.search_query.data, brand=brand))
 
+    # filter products using arguments
     products = Product.query
     if q:
         products = products.filter(Product.name.like(f'%{q}%'))
     if brand:
-        products = products.filter_by(brand=brand).paginate(per_page=6, page=page)
-    else:
-        products = products.paginate(per_page=6, page=page)
+        products = products.filter_by(brand=brand)
+    products = products.paginate(per_page=6, page=page)
 
     return render_template('product/index.html', search_form=search_form,
                            products=products, brands=get_brands(),
