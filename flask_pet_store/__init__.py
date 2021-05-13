73ab3a99261657d89d9316c49d6d9f7163f9cc04
prachurya deepta <deeptoadhikary@gmail.com>
2021-05-13 16:50:34 -0400

    database refactored

--- flask_pet_store/__init__.py
+++ flask_pet_store/__init__.py
@@ -15,7 +15,7 @@ migrate = Migrate()
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
         SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI'),
