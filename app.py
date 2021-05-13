a82e3288fd68c492f6b4145ecd2080e7aebe908f
prachurya deepta <deeptoadhikary@gmail.com>
2021-05-13 15:25:30 -0400

    refactored to application factory

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask_pet_store import app
+from flask_pet_store import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
--- flask_pet_store/__init__.py
+++ flask_pet_store/__init__.py
@@ -7,63 +7,67 @@ from flask_migrate import Migrate
 from flask_principal import Principal, identity_loaded, RoleNeed, UserNeed
 import os
 
-app = Flask(__name__)
-app.config.from_object(os.environ.get('CONFIG_OBJ'))
-
-db = SQLAlchemy(app)
-
-bcrypt = Bcrypt(app)
-
-login_manager = LoginManager(app)
-login_manager.login_view = "customer.login"
-login_manager.login_message_category = "info"
-
-principal = Principal(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+principal = Principal()
+migrate = Migrate()
 
 
-@identity_loaded.connect_via(app)
-def on_identity_loaded(sender, identity):
-    # Set the identity user object
-    identity.user = current_user
+def create_app():
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object(os.environ.get('CONFIG_OBJ'))
+    db.init_app(app)
 
-    # Add the UserNeed to the identity
-    if hasattr(current_user, 'id'):
-        identity.provides.add(UserNeed(current_user.id))
+    bcrypt.init_app(app)
 
-    # Assuming the User model has a list of roles, update the
-    # identity with the roles that the user provides
-    if hasattr(current_user, 'roles'):
-        for role in current_user.roles:
-            identity.provides.add(RoleNeed(role.name))
+    login_manager.init_app(app)
+    login_manager.login_view = "customer.login"
+    login_manager.login_message_category = "info"
 
+    principal.init_app(app)
 
-migrate = Migrate(app, db)
+    migrate.init_app(app, db)
 
-from flask_pet_store.customer.routes import customer_blueprint
-from flask_pet_store.product.routes import product_blueprint
-from flask_pet_store.admin.routes import admin_blueprint
+    with app.app_context():
+        @identity_loaded.connect_via(app)
+        def on_identity_loaded(sender, identity):
+            # Set the identity user object
+            identity.user = current_user
 
-app.register_blueprint(customer_blueprint, url_prefix='/customer')
-app.register_blueprint(product_blueprint, url_prefix='/products')
-app.register_blueprint(admin_blueprint, url_prefix='/admin')
+            # Add the UserNeed to the identity
+            if hasattr(current_user, 'id'):
+                identity.provides.add(UserNeed(current_user.id))
 
+            # Assuming the User model has a list of roles, update the
+            # identity with the roles that the user provides
+            if hasattr(current_user, 'roles'):
+                for role in current_user.roles:
+                    identity.provides.add(RoleNeed(role.name))
 
-# home page
-@app.route('/')
-def home_page():
-    return render_template('index.html')
+        from flask_pet_store.customer.routes import customer_blueprint
+        from flask_pet_store.product.routes import product_blueprint
+        from flask_pet_store.admin.routes import admin_blueprint
 
+        app.register_blueprint(customer_blueprint, url_prefix='/customer')
+        app.register_blueprint(product_blueprint, url_prefix='/products')
+        app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('error_pages/404.html'), 404
+        # home page
+        @app.route('/')
+        def home_page():
+            return render_template('index.html')
 
+        @app.errorhandler(404)
+        def page_not_found(e):
+            return render_template('error_pages/404.html'), 404
 
-@app.errorhandler(403)
-def permission_denied(e):
-    return render_template('error_pages/403.html'), 403
+        @app.errorhandler(403)
+        def permission_denied(e):
+            return render_template('error_pages/403.html'), 403
 
+        @app.errorhandler(500)
+        def server_error(e):
+            return render_template('error_pages/500.html'), 500
 
-@app.errorhandler(500)
-def server_error(e):
-    return render_template('error_pages/500.html'), 500
+        return app
